780b32354c2ea845267fd63a2fdaa79ab21b7768
Emil Rozgonyi <ai.homunculus@gmail.com>
2018-08-22 12:55:29 +0200

    added groups page to templates with all dynamic links

--- students/views.py
+++ students/views.py
@@ -39,7 +39,18 @@ def students_delete(request, sid):
 #Views for Groups
 
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+	groups = (
+	{'id': 1,
+	'group_name': u'Мтм-21',
+	'group_leader': u'Ячменєв Олег'},
+	{'id': 2,
+	'group_name': u'Мтм-22',
+	'group_leader': u'Віталій Подоба'},
+	{'id': 3,
+	'group_name': u'Мтм-23',
+	'group_leader': u'Іванов Андрій'},
+	)
+	return render(request, 'students/groups_list.html', {'groups': groups})
 
 def groups_add(request):
 	return HttpResponse('<h1>Group Add Form</h1>')
