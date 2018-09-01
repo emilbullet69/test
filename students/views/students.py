35d85c0615fd5fb146ce12ef6767fea9ce356672
Emil Rozgonyi <ai.homunculus@gmail.com>
2018-09-01 13:32:33 +0200

    added list sorting+reverse_sort using column headers, added sorting by last_name as default when loading the page, first column now using student(id) instead of forloop.counter, dynamized the pagination

--- students/views/students.py
+++ students/views/students.py
@@ -5,12 +5,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from ..models import Student
 
 #Views for Students
 
 def students_list(request):
-	students = Student.objects.all()
+	students = Student.objects.all().order_by('last_name')
 	header_groups = (
 	{'group': u'МтМ - 21',
 	'leader': u'Подоба Віталій',
@@ -19,6 +21,28 @@ def students_list(request):
 	'leader': u'Корост Андрій',
 	'leader_ticket': 2123},
 	)
+	
+	# try to order students list
+	order_by = request.GET.get('order_by', '')
+	if order_by in ('last_name', 'first_name', 'ticket', 'id'):
+		students = students.order_by(order_by)
+		if request.GET.get('reverse', '') == '1':
+			students = students.reverse()
+	elif request.GET.get('reverse', '') == '1':
+		students = students.reverse()
+
+	# Paginate students
+	paginator = Paginator(students, 3)
+	page = request.GET.get('page')
+	try:
+		students = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		students = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		students = paginator.page(paginator.num_pages)
+
 	return render(request, 'students/students_list.html', {'students': students, 'GROUPS': header_groups})
 
 def students_add(request):
