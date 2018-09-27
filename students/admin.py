86f40c3a4d7d2e3e1c6a66c36082b3c184da2ea1
Emil Rozgonyi <ai.homunculus@gmail.com>
2018-09-27 10:38:20 +0200

    put student and group models to separated files

--- students/admin.py
+++ students/admin.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Student, Group
+from models.students import Student
+
+from models.groups import Group
 
 # Register your models here.
 admin.site.register(Student)
